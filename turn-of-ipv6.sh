da813c88b07f014fdc08d3f1823a19bb75090d31
viva9grup <100474729+viva9grup@users.noreply.github.com>
2022-03-21 12:32:02 +0700

    turn-of-ipv6.sh
    Turn of ipv6

---
+++ turn-of-ipv6.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Turn off ipv6
+#
+
+IPV6=$(ip addr show dev eth0 | sed -e's/^.*inet6 \([^ ]*\)\/.*$/\1/;t;d')
+
+if [[ ! -z ${IPV6} ]];then
+echo "
+	net.ipv6.conf.all.disable_ipv6 = 1
+	net.ipv6.conf.default.disable_ipv6 = 1
+	net.ipv6.conf.lo.disable_ipv6 = 1b" >> /etc/sysctl.conf
+if
